fceaf6a2bf7fb20cd8e6c0a7c9b4045dc3403bdb
Hoàng Minh Thắng <81869874+hoangminhthang2601@users.noreply.github.com>
2024-05-23 17:56:55 +0700

    Update be_product_deploy.sh

--- jenkins/scripts/be_product_deploy.sh
+++ jenkins/scripts/be_product_deploy.sh
@@ -5,7 +5,11 @@ CURRENT_DIR=$PWD
 set -e
     echo "Deploy BE ..."
     cd $WORKING_DIR/jenkins/scripts
+    
+    echo "Migrate database"
+    docker-compose -f docker-compose-be.yml run --rm be sh -c "alembic upgrade head"
 
+    echo "Up services ..."
     docker-compose -f docker-compose-be.yml up -d
 
     cd $CURRENT_DIR
